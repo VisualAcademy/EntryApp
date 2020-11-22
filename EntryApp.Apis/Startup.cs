a1fbb5224ac366a7cfb00868fd4340a04a956a65
RedPlus <redplus@redplus.net>
2020-11-22 22:27:27 +0000

    Code Cleanup

--- EntryApp.Apis/Startup.cs
+++ EntryApp.Apis/Startup.cs
@@ -10,14 +10,10 @@ namespace EntryApp.Apis
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -41,7 +37,6 @@ namespace EntryApp.Apis
             services.AddDependencyInjectionContainerForEntryApp(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
